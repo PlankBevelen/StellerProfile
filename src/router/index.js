ecdc5aeda8dd249fc7cee44413c979bf46795294
plankbevelen <521LuoWenJue...>
2025-05-26 22:52:51 +0800

    initial

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/stellerprofile', name: 'StellerProfile', meta: { title: 'StellerProfile' }, redirect: { name : 'HomeView' }, component: ()=> import('@/views/StellerProfile.vue') , children: [
       { path : 'homeview', name: 'HomeView', meta: { title: 'HomeView' }, component: ()=> import('@/views/HomeView.vue') },
